e512681ce292dc60bf0951c40108317a56a42d93
shahrzad <shahrzadjkoushesh@gmail.com>
2023-11-22 15:48:44 +0330

    add makeRequest function to trigger get req

--- source/src/app/button/button.component.ts
+++ source/src/app/button/button.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+import {environment} from '../../environments/environment.development';
 
 @Component({
   selector: 'app-button',
@@ -6,5 +9,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./button.component.scss']
 })
 export class ButtonComponent {
+
+  response: any;
+  status!: number;
+
+  constructor(private http: HttpClient) {}
+
+  makeRequest() {
+    this.http.get(environment.apiUrl + '/usertest').subscribe(
+      (data: any) => {
+        this.response = data; 
+      },
+      (error) => {
+        console.error('Request failed:', error);
+      },
+      () => {
+        this.status = this.response?.status;
+      }
+    );
+  }
 
 }
