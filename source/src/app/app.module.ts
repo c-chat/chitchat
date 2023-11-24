7a612d7899a82187d59683680a1f48a833b71703
shahrzad <shahrzadjkoushesh@gmail.com>
2023-11-24 12:21:24 +0330

    add chitchat name

--- source/src/app/app.module.ts
+++ source/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { ChitchatNameComponent } from './chitchat-name/chitchat-name.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
+    ChitchatNameComponent,
   ],
   imports: [
     BrowserModule,
