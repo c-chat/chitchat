802e0e57aa7cad6b84b55e0e05271c17db8ecd6a
shahrzad <shahrzadjkoushesh@gmail.com>
2023-11-26 12:19:26 +0330

    fix linter error

--- source/src/app/singin-page/singin-page.component.ts
+++ source/src/app/singin-page/singin-page.component.ts
@@ -3,7 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-singin-page',
   templateUrl: './singin-page.component.html',
-  styleUrls: ['./singin-page.component.scss']
 })
 export class SinginPageComponent {
 
