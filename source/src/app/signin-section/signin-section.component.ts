1d483fe271b675ce3e053d9122996b9e4a634ec7
shahrzad <shahrzadjkoushesh@gmail.com>
2023-12-07 19:35:56 +0330

    add encryption

--- source/src/app/signin-section/signin-section.component.ts
+++ source/src/app/signin-section/signin-section.component.ts
@@ -1,7 +1,9 @@
 import { Component } from '@angular/core';
 import { ValidatorFn } from '@angular/forms';
 import { FormBuilder, FormGroup, AbstractControl, ValidationErrors, Validators, FormControl, FormArray } from '@angular/forms';
-
+import { EncryptionService } from '../encryption.service';
+import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment.development';
 
 @Component({
   selector: 'app-signin-section',
@@ -9,14 +11,24 @@ import { FormBuilder, FormGroup, AbstractControl, ValidationErrors, Validators, 
   styleUrls: ['./signin-section.component.scss']
 })
 export class SigninSectionComponent {
-  constructor (private fb: FormBuilder){}
+  constructor (private fb: FormBuilder, private encryptionService: EncryptionService, private http: HttpClient){}
   signinForm = this.fb.group({
     username: ['', Validators.required],
     password: ['', Validators.required],
   })
 
-  onSubmit() {
-    console.log(this.signinForm)
+  onSubmit(username: string, password: string) {
+    console.log(this.signinForm);
+    const encryptedCredentials = this.encryptionService.encryptCredentials(username, password);
+
+    this.http.post(`${environment.apiUrl}/login`, { credentials: encryptedCredentials }).subscribe(
+      (response) => {
+        
+      },
+      (error) => {
+      
+      }
+    );
   }
 
 }
