93208925208e15a506bda687ac743e3a24a4ea98
shahrzad <shahrzadjkoushesh@gmail.com>
2023-12-07 13:42:47 +0330

    ensure password has small capital letter, min 8 length and a .

--- source/src/app/signup-section/signup-section.component.ts
+++ source/src/app/signup-section/signup-section.component.ts
@@ -11,9 +11,27 @@ export class SignupSectionComponent {
   constructor (private fb: FormBuilder){}
   signupForm = this.fb.group({
     username: ['', Validators.required],
-    password: ['', Validators.required],
+    password: ['', [Validators.required, Validators.minLength(8), this.patternValidator(/\d/, { hasNumber: true }), this.patternValidator(/[A-Z]/, { hasCapitalCase: true }), this.patternValidator(/[a-z]/, { hasSmallCase: true }), this.patternValidator(/[.]+/, { hasSpecialCharacters: true })]],
     confirmPassword: ['', Validators.required]
   })
+
+  patternValidator(regex: RegExp, error: ValidationErrors | null): ValidatorFn {
+    return (control: AbstractControl) => {
+      if (!control.value) {
+        return null;
+      }
+
+      // test the value of the control against the regexp supplied
+      const valid = regex.test(control.value);
+
+      if (valid) {
+        return null;
+      }
+      else {
+        return error;
+      }
+    };
+  }
 
   onSubmit() {
     console.log(this.signupForm)
