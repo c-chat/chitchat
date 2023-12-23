b9a30e4ed8666becd46428daa57ec3c1c80b4c9a
shahrzad <shahrzadjkoushesh@gmail.com>
2023-12-23 13:18:36 +0330

    add additional tests for signin section

--- source/src/app/signin-section/signin-section.component.spec.ts
+++ source/src/app/signin-section/signin-section.component.spec.ts
@@ -30,4 +30,46 @@ describe('SigninSectionComponent', () => {
     expect(component.signinForm.get('username')?.value).toEqual('');
     expect(component.signinForm.get('password')?.value).toEqual('');
   })
+
+  it('should show username error if whitespace is present', () => {
+    const usernameField = component.signinForm.get('username');
+    usernameField?.setValue('user name');
+    usernameField?.markAsTouched();
+    component.onUsernameFieldBlur();
+    expect(component.showUsernameError).toBeTruthy();
+  });
+
+  it('should call onSubmit method when form is submitted', () => {
+    spyOn(component, 'onSubmit');
+    const form = fixture.nativeElement.querySelector('form');
+    form.dispatchEvent(new Event('ngSubmit'));
+    fixture.detectChanges();
+    expect(component.onSubmit).toHaveBeenCalled();
+  });
+
+  it('should show password error when criteria are not met', () => {
+    const passwordControl = component.signinForm.get('password');
+  
+    passwordControl?.setValue('weakpassword');
+  
+    component.onPasswordFieldBlur();
+  
+    expect(component.showPasswordError).toBeTruthy();
+  });
+
+  it('should show error for empty username or password', () => {
+    const usernameControl = component.signinForm.get('username');
+    const passwordControl = component.signinForm.get('password');
+  
+    usernameControl?.setValue('');
+    passwordControl?.setValue('');
+  
+    component.onUsernameFieldBlur();
+    component.onPasswordFieldBlur();
+  
+    expect(usernameControl?.hasError('required')).toBe(true);
+    expect(passwordControl?.hasError('required')).toBe(true);
+  });
+  
+  
 });
