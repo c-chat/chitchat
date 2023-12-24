635f83d0ab0ae139e35f5f2906252a82e16bf799
shahrzad <shahrzadjkoushesh@gmail.com>
2023-12-24 13:49:28 +0330

    dummy

--- source/src/app/chats-page/chats-page.component.ts
+++ source/src/app/chats-page/chats-page.component.ts
@@ -3,7 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-chats-page',
   templateUrl: './chats-page.component.html',
-  styleUrls: ['./chats-page.component.scss']
 })
 export class ChatsPageComponent {
 
