8c09520c5210b561a0bfbdf0db0968534e7569c1
shahrzad <shahrzadjkoushesh@gmail.com>
2023-12-26 23:22:55 +0330

    fix unit tests' errors

--- source/src/app/chats-page/chats-page.component.spec.ts
+++ source/src/app/chats-page/chats-page.component.spec.ts
@@ -1,6 +1,8 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ChatsPageComponent } from './chats-page.component';
+import { ChatsListComponent } from '../chats-list/chats-list.component';
+import { ChatsHeaderComponent } from '../chats-header/chats-header.component';
 
 describe('ChatsPageComponent', () => {
   let component: ChatsPageComponent;
@@ -8,7 +10,7 @@ describe('ChatsPageComponent', () => {
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      declarations: [ChatsPageComponent]
+      declarations: [ChatsPageComponent, ChatsListComponent, ChatsHeaderComponent],
     });
     fixture = TestBed.createComponent(ChatsPageComponent);
     component = fixture.componentInstance;
