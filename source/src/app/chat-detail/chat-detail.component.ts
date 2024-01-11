735d38a318038bdd716a508dccf1ee150c230b05
shahrzad <shahrzadjkoushesh@gmail.com>
2024-01-11 15:01:49 +0330

    sengin messages

--- source/src/app/chat-detail/chat-detail.component.ts
+++ source/src/app/chat-detail/chat-detail.component.ts
@@ -7,12 +7,12 @@ import { ChatService } from '../chat.service';
   templateUrl: './chat-detail.component.html',
   styleUrls: ['./chat-detail.component.scss']
 })
-export class ChatDetailComponent implements OnInit{
+export class ChatDetailComponent implements OnInit {
 
   chatId!: number;
   chatData: any = { user: {}, messages: [] };
 
-  constructor (private route: ActivatedRoute, private router: Router, private chatService: ChatService) {}
+  constructor(private route: ActivatedRoute, private router: Router, private chatService: ChatService) {}
 
   ngOnInit(): void {
     this.route.params.subscribe(params => {
@@ -37,8 +37,19 @@ export class ChatDetailComponent implements OnInit{
     );
   }
 
+  onMessageSent(message: string) {
+    const senderId = 1; 
+    const receiverId = 2; 
+
+    const newMessage = {
+      id: senderId,
+      content: message
+    };
+
+    this.chatData.messages.push(newMessage);
+  }
+
   goBack(): void {
     this.router.navigate(['/chats']);
   }
-
 }
--- source/src/app/send-message/send-message.component.ts
+++ source/src/app/send-message/send-message.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-send-message',
@@ -6,5 +6,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./send-message.component.scss']
 })
 export class SendMessageComponent {
+  @Output() messageSent = new EventEmitter<string>();
+  message: string = '';
 
+  sendMessage() {
+    if (this.message.trim() !== '') {
+      console.log(this.message)
+      this.messageSent.emit(this.message.trim());
+      this.message = ''; 
+    }
+  }
 }
